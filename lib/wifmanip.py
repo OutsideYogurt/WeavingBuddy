31dac34d4c3e82ce76b210ea5bb784de6691f682
OutsideYogurt <71948034+OutsideYogurt@users.noreply.github.com>
2020-10-03 17:35:42 -0400

    reading category inside wif

--- lib/wifmanip.py
+++ lib/wifmanip.py
@@ -3,8 +3,61 @@ import lib.filemanip as fm
 class wifmanip():
 
     def __init__(self, filepath):
-        self.mywif = fm.filemanip(filepath)
+        self.mywiffile = fm.filemanip(filepath)
+        self.wif = self._fillcategory("[WIF]")
+        self.contents = self._fillcategory("[CONTENTS]")
+        self.weaving = self._fillcategory("[WEAVING]")
+        self.warp = self._fillcategory("[WARP]")
+        self.weft = self._fillcategory("[WEFT]")
+        self.colorpalette = self._fillcategory("[COLOR PALETTE]")
+        self.colortable = self._fillcategory("[COLOR TABLE]")
+        self.threading = self._fillcategory("[THREADING]")
+        self.treadling = self._fillcategory("[TREADLING]")
+        self.tieup = self._fillcategory("[TIEUP]")
+        self.privatepixeloom = self._fillcategory("[PRIVATE PIXELOOM]")
 
 
-    def _getwif(self):
-        self.mywif
+    def _fillcategory(self, categoryname):
+        incategory = False
+        tempdict = {}
+        for line in self.mywiffile.returnlines():
+            if incategory == True:
+                if line[0] == "[":
+                    return tempdict
+                else:
+                    tempdict[line[:line.find("=")]] = line[line.find("=")+1:-1]
+            if line[:-1] == categoryname:
+                incategory = True
+        return tempdict
+
+    def printcategory(self, categoryname):
+        if categoryname.lower() == "wif":
+            self.printdict(self.wif)
+        elif categoryname.lower() == "contents":
+            self.printdict(self.contents)
+        elif categoryname.lower() == "weaving":
+            self.printdict(self.weaving)
+        elif categoryname.lower() == "warp":
+            self.printdict(self.warp)
+        elif categoryname.lower() == "weft":
+            self.printdict(self.weft)
+        elif categoryname.lower() == "color palette":
+            self.printdict(self.colorpalette)
+        elif categoryname.lower() == "color table":
+            self.printdict(self.colortable)
+        elif categoryname.lower() == "threading":
+            self.printdict(self.threading)
+        elif categoryname.lower() == "treadling":
+            self.printdict(self.treadling)
+        elif categoryname.lower() == "tieup":
+            self.printdict(self.tieup)
+        elif categoryname.lower() == "private pixeloom":
+            self.printdict(self.privatepixeloom)
+        else:
+            return False
+
+
+    def printdict(self, dicttoprint):
+        for key in dicttoprint:
+            print(key + " = " + dicttoprint[key])
+
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import lib.wifmanip as wm
 
 currentproject = wm.wifmanip("data/Simple_Example.wif")
+
+currentproject.printcategory('PRIVATE PIXELOOM')
